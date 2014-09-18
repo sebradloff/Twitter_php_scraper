9bed9312f03f29d6340e616e3dbb71ba90ebdf83
Sebastian Radloff <sradloff23@gmail.com>
2014-09-17 21:44:55 -0500

    passed rawHTML as a parameter to the parser model

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -6,10 +6,10 @@ class Welcome extends CI_Controller {
 	public function index() {
 		$this->load->model("scraper");
 		$rawHTML = $this->scraper->scrape();
-
-		$this->load->model("parser");
-		$followerNumber = $this->parser->getFollowerNumber();
 		
+		$this->load->model("parser");
+		$followerNumber = $this->parser->getFollowerNumber($rawHTML);
+
 
 	}
 
