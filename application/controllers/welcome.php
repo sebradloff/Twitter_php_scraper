e28ae79ecde1bd56bdd3cedf2a68b374dee751c0
Sebastian Radloff <sradloff23@gmail.com>
2014-09-17 21:18:00 -0500

    scraped raw html from twitter

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,6 +2,15 @@
 
 class Welcome extends CI_Controller {
 
+
+	public function index() {
+		$this->load->model("scraper");
+
+		$followerNumber = $this->scraper->scrape();
+
+		print_r($followerNumber);
+	}
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -17,10 +26,10 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+	// public function index()
+	// {
+	// 	$this->load->view('welcome_message');
+	// }
 }
 
 /* End of file welcome.php */
--- application/models/scraper.php
+++ application/models/scraper.php
@@ -2,9 +2,24 @@
 
 class Scraper extends CI_Model {
 
-	public function scrape($twitterName){
-		$page = curl_init("https://twitter.com/BarackObama");
-		print $page;
+	public function scrape(){
+		// initializes curl sequence
+		$ch = curl_init();
+
+		// curls site from specific url
+		curl_setopt($ch, CURLOPT_URL, "https://twitter.com/BarackObama");
+		// returns raw html from the curled site
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		// actually curls the site
+		$output = curl_exec($ch);
+
+		// close the curl session
+		curl_close($ch);
+
+
+		
+		echo htmlentities($output);
 	}
 
 }
