b204392f162d30e7b9de3195afaaf0dce3f27f45
Sebastian Radloff <sradloff23@gmail.com>
2014-09-17 22:12:53 -0500

    using the new simple notation from the library to grab the raw html from the page

--- application/models/scraper.php
+++ application/models/scraper.php
@@ -3,22 +3,11 @@
 class Scraper extends CI_Model {
 
 	public function scrape(){
-		// initializes curl sequence
-		$ch = curl_init();
+		include('application/libraries/simple_html_dom.php');
 
-		// curls site from specific url
-		curl_setopt($ch, CURLOPT_URL, "https://twitter.com/BarackObama");
-		// returns raw html from the curled site
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$rawHTML = file_get_html('https://twitter.com/BarackObama');
 
-		// actually curls the site
-		$output = curl_exec($ch);
-
-		// close the curl session
-		curl_close($ch);
-
-		// returns html elements of the page
-		return htmlentities($output);
+		return $rawHTML;
 	}
 
 }
